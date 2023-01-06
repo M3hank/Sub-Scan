9eefe1e1212c697b902f8e7aa088b4b85b9ce314
Mehank <70057473+M3hank@users.noreply.github.com>
2023-01-06 21:31:49 +0500

    Improved Logic

--- Sub-Scan.py
+++ Sub-Scan.py
@@ -9,7 +9,7 @@ print("""\033[1;36m ██████  █    ██  ▄▄▄▄          █
 ░ ▓██▄   ▓██  ▒██░▒██▒ ▄██     ░ ▓██▄  ▒▓█    ▄ ▒██  ▀█▄ ▓██  ▀█ ██▒
   ▒   ██▒▓▓█  ░██░▒██░█▀         ▒   ██▒▓▓▄ ▄██▒░██▄▄▄▄██▓██▒  ▐▌██▒
 ▒██████▒▒▒▒█████▓ ░▓█  ▀█▓     ▒██████▒▒ ▓███▀ ░ ▓█   ▓██▒██░   ▓██░
-▒ ▒▓▒ ▒ ░░▒▓▒ ▒ ▒ ░▒▓███▀▒     ▒ ▒▓▒ ▒ ░ ░▒ ▒  ░ ▒▒   ▓▒█░ ▒░   ▒ ▒ \033[0m""")
+▒ ▒▓▒ ▒ ░░▒▓▒ ▒ ▒ ░▒▓███▀▒     ▒ ▒▓▒ ▒ ░ ░▒ ▒  ░ ▒▒   ▓▒█░ ▒░   ▒ ▒ \033[0;0m""")
 print("")
 
 # Creating a Parser
@@ -32,23 +32,27 @@ max_subdomains = args.max_subdomains
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
 
+printed_subdomains = set()
+
 def sub_brute(subdomain):
     try:
         url = f'http://{subdomain}'
         response = requests.head(url,headers=headers,allow_redirects = True)
         status = response.status_code
-        if status == 200:
-            print(f'\033[92m [+] >> {subdomain}   status-code:[{status}]\033[00m')
-            if output_file:
-                with open(output_file, 'a') as f:
-                    f.write(f'{subdomain}\n')
-        elif status == 301:
-            print(f'\033[93m [+] >> {subdomain}   status-code:[{status}]\033[00m)')
-        elif status == 404:
-            print(f'\033[91m [+] >> {subdomain} status-code:[{status}]\033[00m')
+        final = response.url
+        if final not in printed_subdomains and domain in final:
+            printed_subdomains.add(final)
+            if status == 200:
+                print(f'\033[92m [+] >> {final}   status-code:[{status}]\033[00m')
+                if output_file:
+                    with open(output_file, 'a') as f:
+                        f.write(f'{final}\n')
+            elif status == 301:
+                print(f'\033[93m [+] >> {final}   status-code:[{status}]\033[00m)')
+            elif status == 404:
+                print(f'\033[91m [+] >> {final} status-code:[{status}]\033[00m')
     except Exception as e:
         pass
-
 def main():
     with open(wordlist, 'r') as f:
         subdomains = [f'{line.strip()}.{domain}' for line in f]
